3aeb0f4127db35c9681603104c7eff4c5ecf271d
Konrad <konrad9009@gmail.com>
2015-06-08 14:00:02 +0200

    initial commit, added TestSpec.m (Specta)

---
+++ TestSpec.m
@@ -0,0 +1,24 @@
+#parse("C File Header.h")
+
+#import <Specta/Specta.h>
+#import <Expecta/Expecta.h>
+
+SpecBegin(${NAME}Spec)
+
+    describe(@"add description...", ^{
+    
+        beforeEach(^{
+        
+        });
+
+        afterEach(^{
+        
+        });
+       
+        it(@"should...", ^{
+
+        });
+    
+    });
+
+SpecEnd
